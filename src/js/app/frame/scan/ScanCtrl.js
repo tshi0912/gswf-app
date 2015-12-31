7f4253ebd78e935672285d646ffdfdbe3e6753fd
Tao Shi <tshi0912@gmail.com>
2015-12-31 16:04:56 +0800

    +

--- src/js/app/frame/scan/ScanCtrl.js
+++ src/js/app/frame/scan/ScanCtrl.js
@@ -4,10 +4,23 @@
 (function () {
     appModule('gswf.frame')
         .controller('ScanCtrl', ScanCtrl);
-    ScanCtrl.$inject = ['$scope','$ionicHistory'];
+    ScanCtrl.$inject = ['$scope', '$cordovaBarcodeScanner'];
 
-    function ScanCtrl($scope, $ionicHistory) {
+    function ScanCtrl($scope, $cordovaBarcodeScanner) {
         console.log('ScanCtrl');
         var vm = this;
+
+        vm.scan = function () {
+            $cordovaBarcodeScanner.scan()
+                .then(function (data) {
+                    alert(data.text);
+                    console.log("Barcode Format -> " + data.format);
+                    console.log("Cancelled -> " + data.cancelled);
+                }, function (error) {
+                    console.log("An error happened -> " + error);
+                });
+        };
+
+        vm.scan();
     }
 })();
